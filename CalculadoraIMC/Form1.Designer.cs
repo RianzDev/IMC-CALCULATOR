c1d28cda2398375d177c0b733937635b4fcb82c3
main <riandasilva2@gmail.com>
2024-03-23 22:37:05 -0300

    Created Project

---
+++ CalculadoraIMC/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace CalculadoraIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            this.button1IMC = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelWeight = new System.Windows.Forms.Label();
+            this.labelHeight = new System.Windows.Forms.Label();
+            this.textBoxWeight = new System.Windows.Forms.TextBox();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = System.Drawing.Color.White;
+            label1.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(177)))), ((int)(((byte)(223)))));
+            label1.Location = new System.Drawing.Point(72, 23);
+            label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(186, 30);
+            label1.TabIndex = 9;
+            label1.Text = "CALCULE SEU IMC";
+            label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1IMC
+            // 
+            this.button1IMC.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(177)))), ((int)(((byte)(223)))));
+            this.button1IMC.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1IMC.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1IMC.ForeColor = System.Drawing.Color.White;
+            this.button1IMC.Location = new System.Drawing.Point(90, 228);
+            this.button1IMC.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1IMC.Name = "button1IMC";
+            this.button1IMC.Size = new System.Drawing.Size(145, 44);
+            this.button1IMC.TabIndex = 5;
+            this.button1IMC.Text = "CALCULAR";
+            this.button1IMC.UseMnemonic = false;
+            this.button1IMC.UseVisualStyleBackColor = false;
+            this.button1IMC.Click += new System.EventHandler(this.button1IMC_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::CalculadoraIMC.Properties.Resources.simplistic_water_bottle;
+            this.pictureBox3.Location = new System.Drawing.Point(61, 125);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(31, 71);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CalculadoraIMC.Properties.Resources.sammy_girl_works_out_on_a_gym_bike;
+            this.pictureBox2.Location = new System.Drawing.Point(-31, 70);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(123, 126);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::CalculadoraIMC.Properties.Resources.icons8_x_48;
+            this.pictureBox1.Location = new System.Drawing.Point(295, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(17, 13);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // labelWeight
+            // 
+            this.labelWeight.AutoSize = true;
+            this.labelWeight.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWeight.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(177)))), ((int)(((byte)(223)))));
+            this.labelWeight.Location = new System.Drawing.Point(115, 95);
+            this.labelWeight.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelWeight.Name = "labelWeight";
+            this.labelWeight.Size = new System.Drawing.Size(113, 21);
+            this.labelWeight.TabIndex = 13;
+            this.labelWeight.Text = "Insira seu peso";
+            // 
+            // labelHeight
+            // 
+            this.labelHeight.AutoSize = true;
+            this.labelHeight.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHeight.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(177)))), ((int)(((byte)(223)))));
+            this.labelHeight.Location = new System.Drawing.Point(115, 125);
+            this.labelHeight.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelHeight.Name = "labelHeight";
+            this.labelHeight.Size = new System.Drawing.Size(120, 21);
+            this.labelHeight.TabIndex = 14;
+            this.labelHeight.Text = "Insira sua altura";
+            // 
+            // textBoxWeight
+            // 
+            this.textBoxWeight.Location = new System.Drawing.Point(232, 98);
+            this.textBoxWeight.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxWeight.Name = "textBoxWeight";
+            this.textBoxWeight.Size = new System.Drawing.Size(26, 20);
+            this.textBoxWeight.TabIndex = 15;
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.AcceptsReturn = true;
+            this.textBoxHeight.Location = new System.Drawing.Point(232, 122);
+            this.textBoxHeight.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(26, 20);
+            this.textBoxHeight.TabIndex = 16;
+            // 
+            // labelResult
+            // 
+            this.labelResult.Enabled = false;
+            this.labelResult.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.ForeColor = System.Drawing.Color.Black;
+            this.labelResult.Location = new System.Drawing.Point(115, 164);
+            this.labelResult.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(120, 21);
+            this.labelResult.TabIndex = 17;
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(311, 280);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.textBoxWeight);
+            this.Controls.Add(this.labelHeight);
+            this.Controls.Add(this.labelWeight);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.button1IMC);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora IMC";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        public System.Windows.Forms.Button button1IMC;
+        private System.Windows.Forms.Label labelWeight;
+        private System.Windows.Forms.Label labelHeight;
+        private System.Windows.Forms.TextBox textBoxWeight;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ CalculadoraIMC/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraIMC
+{
+    public partial class Form1 : Form
+    {
+        // Método para obter a altura digitada no textBoxHeight e convertê-la em metros
+        public double GetHeight()
+        {
+            // Verifica se o campo de altura não está vazio e se é um valor numérico válido
+            if (!string.IsNullOrEmpty(textBoxHeight.Text) && double.TryParse(textBoxHeight.Text, out double height))
+                // Retorna a altura convertida para metros
+                return (double)Convert.ToDecimal(height) / 100;
+            else
+                // Se o valor digitado não for válido, lança uma exceção
+                throw new FormatException("O valor de altura não é válido.");
+        }
+
+        // Método para obter o peso digitado no textBoxWeight
+        public double GetWeight()
+        {
+            // Verifica se o campo de peso não está vazio e se é um valor numérico válido
+            if (!string.IsNullOrEmpty(textBoxWeight.Text) && double.TryParse(textBoxWeight.Text, out double weight))
+                // Retorna o peso
+                return weight;
+            else
+                // Se o valor digitado não for válido, lança uma exceção
+                throw new FormatException("O valor de peso não é válido.");
+        }
+
+        // Construtor da classe Form1
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Evento de clique no pictureBox1 (fechar aplicação)
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Close(); // Fecha a aplicação
+        }
+
+        // Evento de clique no botão de calcular IMC
+        private void button1IMC_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Calcula o IMC usando os métodos GetWeight e GetHeight e exibe o resultado em uma MessageBox
+                double result = GetWeight() / (GetHeight() * GetHeight());
+                MessageBox.Show($"Vale lembrar que o IMC ideal pode variar de pessoa para pessoa, dependendo da composição corporal e do nível de atividade física. Consultar um profissional de saúde é essencial para uma avaliação personalizada e precisa.", "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Habilita o rótulo que exibirá o resultado do IMC e atribui o valor calculado
+                labelResult.Enabled = true;
+                labelResult.Text = result.ToString("#.##"); // Formata o resultado com duas casas decimais
+            }
+            catch (FormatException ex)
+            {
+                // Em caso de exceção de formato (quando os valores digitados não são válidos), exibe uma mensagem de erro
+                MessageBox.Show(ex.Message, "Erro de Formato", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
